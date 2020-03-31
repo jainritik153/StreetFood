8b9ce4b530745928d438a3935801b57bd57f217a
ritik <jainritik153@gmail.com>
2020-03-31 20:45:49 +0530

    Commit check

--- screens/explore.js
+++ screens/explore.js
@@ -21,6 +21,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 const header_height = StatusBar.currentHeight;
 
+//comment kia hai yeh line
 const Stack = createStackNavigator();
 //changed class component to function component
 function Explore() {
