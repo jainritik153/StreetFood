feb7e48ef0dba963061d7a8dba0697ed2a704c50
ritik <jainritik153@gmail.com>
2020-04-12 22:50:22 +0530

    Added marker to map

--- screens/mapScreen.js
+++ screens/mapScreen.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 
-export default function App () {
-    return (
-      <MapView
-         style={{ flex: 1 }}
-         provider={PROVIDER_GOOGLE}
-         showsUserLocation
-         initialRegion={{
-         latitude: 19.380775, 
-         longitude: 72.829487,
-         latitudeDelta: 0.0922,
-         longitudeDelta: 0.0421}}
-      />
-    );
-  
-}
+import React, { useState } from "react";
 
+import MapView, { Marker } from "react-native-maps";
+
+function MapScreen (){
+  const [region, setRegion] = useState({
+    latitude: 19.380775,
+    longitude: 72.829487,
+    latitudeDelta: 0.009,
+    longitudeDelta: 0.009
+  });
+
+  return (
+    <MapView
+      style={{ flex: 1 }}
+      region={region}
+      onRegionChangeComplete={region => setRegion(region)}
+    >
+      <Marker coordinate={{ latitude: 19.380775, longitude: 72.829487 }} />
+    </MapView>
+  );
+};
+
+export default MapScreen;
